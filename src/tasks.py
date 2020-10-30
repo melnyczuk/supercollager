99e7854d35a4bff5ffd9aaf47df5888a7326a079
how melnyczuk <how@melnycz.uk>
2020-10-30 11:55:16 +0000

    format, lint & typecheck

--- src/tasks.py
+++ src/tasks.py
@@ -1,9 +1,9 @@
-from typing import Dict
+from typing import Any, Dict
 
 import src.app as app
 from .celery import celery
 
 
 @celery.task  # type: ignore
-def a_task(data: Dict[str, any]) -> int:
+def a_task(data: Dict[str, Any]) -> int:
     return 2 * 2
