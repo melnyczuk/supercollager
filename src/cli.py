444792c38256c9e92d094ac5752abe1f245997ef
how melnyczuk <how@melnycz.uk>
2020-11-13 14:04:24 +0000

    Add CLI save decorator

--- src/cli.py
+++ src/cli.py
@@ -1,10 +1,32 @@
-from src.pipelines import blocks, collage, segment
+from datetime import datetime
+
+from . import pipelines
+from .app.io import IO
+
+
+def save(fn):
+    @staticmethod
+    def f(*args, **kwargs):
+        now = datetime.now().strftime("%Y%m%d-%H:%M:%S")
+        dir = kwargs.pop("dir", "./dump/output")
+        fname = kwargs.pop("fname", now)
+        ext = kwargs.pop("ext", "png")
+
+        for i, img in enumerate(fn(*args, **kwargs)):
+            IO.save.image(
+                img,
+                fname=f"{fname}-{i}",
+                dir=dir,
+                ext=ext,
+            )
+
+    return f
 
 
 class CLI:
-    blocks = blocks
-    collage = collage
-    segment = segment
+    blocks = save(pipelines.blocks)
+    collage = save(pipelines.collage)
+    segment = save(pipelines.segment)
 
 
 if __name__ == "__main__":
