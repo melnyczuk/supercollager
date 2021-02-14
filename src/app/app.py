1443681d79d0becc90a3f4d5fda002790c5894ff
how melnyczuk <how@melnycz.uk>
2021-02-14 19:15:04 +0000

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -5,11 +5,11 @@ from tqdm.std import tqdm  # type: ignore
 
 from src.app.composition import Composition
 from src.app.load import Load
+from src.app.image_type import ImageType
 from src.app.masking import Masking
 from src.app.post_process import PostProcess
 from src.app.roi import ROI
 from src.app.segmentation import Segmentation
-from src.app.types import ImageType, LabelImage
 from src.logger import logger
 
 
@@ -18,25 +18,15 @@ class App:
     def segment(
         uris: List[str],
         rotate: Union[float, bool] = False,
-    ) -> List[LabelImage]:
+    ) -> List[ImageType]:
         imgs = Load.uris(uris)
         logger.log(f"loaded {len(imgs)} images")
 
         logger.log("analysing images:")
 
-        analysed_images = Segmentation().mask_rcnn(imgs)
-        logger.log(
-            f"found {len(analysed_images)} segments in {len(imgs)} URIs finding {len(set(ai.label for ai in analysed_images))} different types of object"  # noqa: E501
-        )
-
-        logger.log("segmenting images:")
-        return [
-            LabelImage(
-                img=App.__apply_segmentation(ai.img, ai.mask, rotate),
-                label=ai.label,
-            )
-            for ai in tqdm(analysed_images)
-        ]
+        segments = Segmentation().mask_rcnn(imgs, rotate=rotate)
+        logger.log(f"found {len(segments)} segments in {len(imgs)} URIs")
+        return segments
 
     @staticmethod
     def collage(
@@ -49,8 +39,3 @@ class App:
         comp = Composition.layer_images(imgs=imgs, background=bg)
         post = PostProcess(comp).contrast(1.2).color(1.2)
         return ImageType(post.img)
-
-    @staticmethod
-    def __apply_segmentation(img, mask, rotate):
-        masked_img = Masking.apply_mask(img, mask, rotate=rotate)
-        return ROI.crop(masked_img)
