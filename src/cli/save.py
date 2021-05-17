bbf9869804250b68822103bf6d791df23212ada2
how melnyczuk <how@melnycz.uk>
2021-05-17 18:16:56 +0200

    refactor save

--- src/cli/save.py
+++ src/cli/save.py
@@ -1,5 +1,5 @@
 import os
-from typing import Iterable, Optional, Union
+from typing import Iterable
 
 import numpy as np
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip  # type: ignore
@@ -11,67 +11,69 @@ from src.constants import VALID_EXTS
 class Save:
     fname: str
     dir: str
+    count: int
 
-    def jpg(self: "Save", img: np.ndarray, index: Optional[int] = None) -> None:
-        self.__image(img, ext="jpg", index=index)
+    def jpg(self: "Save", *imgs: np.ndarray, **kwargs) -> None:
+        self.__image(*imgs, ext="jpg", **kwargs)
         return
 
-    def png(self: "Save", img: np.ndarray, index: Optional[int] = None) -> None:
-        self.__image(img, ext="png", index=index)
+    def png(self: "Save", *imgs: np.ndarray, **kwargs) -> None:
+        self.__image(*imgs, ext="png", **kwargs)
         return
 
-    def mp4(
-        self: "Save",
-        clip: Iterable[np.ndarray],
-        **kwargs,
-    ) -> None:
-        self.__video(clip, ext="mp4", **kwargs)
+    def mp4(self: "Save", *clips: Iterable[np.ndarray], **kwargs) -> None:
+        self.__video(*clips, ext="mp4", **kwargs)
         return
 
-    def avi(
-        self: "Save",
-        clip: Iterable[np.ndarray],
-        **kwargs,
-    ) -> None:
-        self.__video(clip, ext="avi", **kwargs)
+    def avi(self: "Save", *clips: Iterable[np.ndarray], **kwargs) -> None:
+        self.__video(*clips, ext="avi", **kwargs)
         return
 
-    def webm(
-        self: "Save",
-        clip: Iterable[np.ndarray],
-        **kwargs,
-    ) -> None:
-        self.__video(clip, ext="webm", **kwargs)
-        return
-
-    def __video(
-        self: "Save",
-        clip: Iterable[np.ndarray],
-        ext: str,
-        fps: int,
-        **kwargs,
-    ):
-        fpath = f"{os.path.join(self.dir, Save.__remove_ext(self.fname))}.{ext}"
-        ImageSequenceClip(
-            [np.dstack((m, m, m)) for m in clip],  # type: ignore
-            with_mask=False,
-            fps=fps,
-        ).write_videofile(fpath, **kwargs)
+    def webm(self: "Save", *clips: Iterable[np.ndarray], **kwargs) -> None:
+        self.__video(*clips, ext="webm", **kwargs)
         return
 
     def __image(
         self: "Save",
-        img: np.ndarray,
+        *imgs: np.ndarray,
         ext: str,
-        index: Optional[int] = None,
-    ):
-        name = Save.__remove_ext(self.fname) + Save.__maybe_append(index)
-        Image.fromarray(img.astype(np.uint8)).save(
-            f"{os.path.join(self.dir, name)}.{ext}"
-        )
+        **kwargs,
+    ) -> None:
+        for img in imgs:
+            Image.fromarray(img.astype(np.uint8)).save(
+                self.__get_path(ext),
+                **kwargs,
+            )
+            self.__increment()
         return
 
-    def __init__(self: "Save", fname=None, dir=None):
+    def __video(
+        self: "Save",
+        *clips: Iterable[np.ndarray],
+        ext: str,
+        fps: int,
+        **kwargs,
+    ) -> None:
+        for clip in clips:
+            ImageSequenceClip(
+                [np.dstack((m, m, m)) for m in clip],  # type: ignore
+                with_mask=False,
+                fps=fps,
+            ).write_videofile(self.__get_path(ext), **kwargs)
+            self.__increment()
+        return
+
+    def __get_path(
+        self: "Save",
+        ext: str,
+    ) -> str:
+        fname = f"{Save.__remove_ext(self.fname)}-{self.count}"
+        return f"{os.path.join(self.dir, fname)}.{ext}"
+
+    def __increment(self: "Save") -> None:
+        self.count += 1
+
+    def __init__(self: "Save", fname=None, dir=None) -> None:
         if not fname:
             raise ValueError("Please provide file name")
         if not dir:
@@ -80,6 +82,8 @@ class Save:
             os.mkdir(dir)
         self.fname = fname
         self.dir = dir
+        self.count = 0
+        return
 
     @staticmethod
     def __remove_ext(fname: str) -> str:
@@ -87,7 +91,3 @@ class Save:
             if ext in fname:
                 return fname.split(f".{ext}")[0]
         return fname
-
-    @staticmethod
-    def __maybe_append(something: Union[str, int, None], sep: str = "-") -> str:
-        return f"{sep}{something}" if something else ""
