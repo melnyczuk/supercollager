108356995799ca7dee086c7f976cd3b87f2ae539
how melnyczuk <how@melnycz.uk>
2021-05-17 18:16:35 +0200

    Refactor adapters

--- src/adapter/adapter.py
+++ src/adapter/adapter.py
@@ -3,49 +3,38 @@ from io import BytesIO
 from typing import Any, Iterable
 
 import numpy as np
+import requests
 from moviepy.video.io.VideoFileClip import VideoFileClip  # type: ignore
 from PIL import Image  # type: ignore
 
 from src.adapter.directory import DirectoryAdapter
 from src.adapter.local_file import LocalFileAdapter
-from src.adapter.url import UrlAdapter
+from src.logger import logger
 
 
 class Adapter:
     @staticmethod
-    def load_one(inp: str, img_mode: str = "RGB") -> np.ndarray:
-        file = Adapter.__match(inp)
-        img = Image.open(file).convert(img_mode)
-        return np.array(img, dtype=np.uint8)
-
-    @staticmethod
-    def load(
-        inputs: Iterable[str],
-        img_mode: str = "RGB",
-    ) -> Iterable[np.ndarray]:
-        files = (file for inp in inputs for file in Adapter.__match(inp))
+    def load(*inp: str, img_mode: str = "RGB") -> Iterable[np.ndarray]:
+        files = (file for i in inp for file in Adapter.__match(i))
         imgs = (Image.open(file).convert(img_mode) for file in files)
         return (np.array(img, dtype=np.uint8) for img in imgs)
 
     @staticmethod
-    def video(input: str) -> VideoFileClip:
-        return VideoFileClip(input)
+    def video(*inp: str) -> Iterable[VideoFileClip]:
+        for i in inp:
+            try:
+                yield VideoFileClip(i)
+            except OSError as e:
+                logger.error(str(e))
 
     @staticmethod
-    def __match(
-        input: Any,
-    ) -> Iterable[BytesIO]:
-        if os.path.isfile(input):
-            if data := LocalFileAdapter.load(input):
+    def __match(inp: Any) -> Iterable[BytesIO]:
+        if os.path.isfile(inp):
+            if data := LocalFileAdapter.load(inp):
                 yield data
-            pass
-        if os.path.isdir(input):
-            for data in DirectoryAdapter.load(input):
+        if os.path.isdir(inp):
+            for data in DirectoryAdapter.load(inp):
                 if data:
                     yield data
-            pass
-        if input.startswith("http"):
-            for data in UrlAdapter.scrape_site(input):
-                yield data
-            pass
-        pass
+        if inp.startswith("http"):
+            yield BytesIO(requests.get(inp).content)
--- src/adapter/local_file.py
+++ src/adapter/local_file.py
@@ -7,10 +7,10 @@ from src.constants import VALID_EXTS
 
 class LocalFileAdapter:
     @staticmethod
-    def load(path: str) -> Optional[BytesIO]:
-        if not os.path.isfile(path):
+    def load(inp: str) -> Optional[BytesIO]:
+        if not os.path.isfile(inp):
             raise ValueError("input is not local")
-        if path.lower().split(".")[-1] not in VALID_EXTS:
+        if inp.lower().split(".")[-1] not in VALID_EXTS:
             return None
-        with open(path, "rb") as f:
+        with open(inp, "rb") as f:
             return BytesIO(f.read())
